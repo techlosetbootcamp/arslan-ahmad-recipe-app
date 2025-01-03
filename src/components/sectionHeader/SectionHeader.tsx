b65bb4b479c72a5d807b7f6866e5991efccd1e8b
ARslan_Ahm <mycomputer2214@gmail.com>
2025-01-03 18:53:16 +0500

    [fix] resolve local-remote conflics and ready to deploy.

--- src/components/sectionHeader/SectionHeader.tsx
+++ src/components/sectionHeader/SectionHeader.tsx
@@ -6,7 +6,7 @@ const SectionHeader: React.FC<SectionHeaderProps> = ({ title, alignment }) => {
       <h2
         className={`text-[32px] ${
           alignment == "left" &&
-          "text-center min-[1024px]:text-left min-[1024px]:ml-28 min-[1281px]:ml-10"
+          "text-center min-[1024px]:text-left min-[1024px]:ml-28 min-[1281px]:ml-10 min-[1535px]:ml-40"
         } md:text-[38.9px] leading-[47.07px] my-10 font-[700] text-gray-800`}
       >
         {title}
